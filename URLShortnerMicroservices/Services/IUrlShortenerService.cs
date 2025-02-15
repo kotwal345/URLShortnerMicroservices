e49d24c847062905b81b8eb7d2762c7b8b975cef
kotwal345 <rushikeshkotwal345@gmail.com>
2025-02-15 12:19:56 +0530

    Added interface

---
+++ URLShortnerMicroservices/Services/IUrlShortenerService.cs
@@ -0,0 +1,22 @@
+﻿namespace URLShortnerMicroservices.Services
+{
+    /// <summary>
+    /// Interface layer for the URL Service.
+    /// </summary>
+    public interface IUrlShortenerService
+    {
+        /// <summary>
+        /// Method which will perform the shortning of the long url provided.
+        /// </summary>
+        /// <param name="originalUrl">Instance Of string</param>
+        /// <returns>Instance of string indicating shortned url for longUrl. </returns>
+        Task<string> ShortenUrlAsync(string originalUrl);
+
+        /// <summary>
+        /// Method will return the valid long url for given shortUrl.
+        /// </summary>
+        /// <param name="shortCode">Instance of string</param>
+        /// <returns>Instance of string representing the long url for given short URL. </returns>
+        Task<string?> GetOriginalUrlAsync(string shortCode);
+    }
+}
