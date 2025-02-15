5c381f2bc0dcf74fa5958a4426740786e0499d8f
kotwal345 <rushikeshkotwal345@gmail.com>
2025-02-15 13:05:51 +0530

    Added for Shorten Url Method .

---
+++ URLShortnerMicroservices/Model/urlMapping.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortnerMicroservices.Model
+{
+    public class urlMapping
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string shortUrl { get; set; } = string.Empty;
+
+        [Required]
+        public string longUrl { get; set; } = string.Empty;
+
+    }
+}
---
+++ URLShortnerMicroservices/Services/URLShortenerService.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using URLShortnerMicroservices.Model;
+
+namespace URLShortnerMicroservices.Services
+{
+    public class URLShortenerService : IUrlShortenerService
+    {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        private Random _random = new Random();
+        public Task<string?> GetOriginalUrlAsync(string shortCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ShortenUrlAsync(string originalUrl)
+        {
+
+            //generate short code
+            var shortcode = GenerateShortCode();
+            //Add prefix if nedded
+            var shortUrl = "newgen.ly" + shortcode;
+            //add into database
+            var mapping = new urlMapping();
+            mapping.shortUrl = shortUrl;
+            mapping.longUrl = originalUrl;
+
+            
+            throw new NotImplementedException();
+        }
+        private string GenerateShortCode(int length = 6)
+        {
+            return new string(Enumerable.Repeat(Alphabet, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
